efff1197371c2983f4202b2e291b451a46a3b12c
TiagoLobo-source <65892221+TiagoLobo-source@users.noreply.github.com>
2023-10-07 12:03:17 +0100

    Update details product working, filtered list of products by id done, logout routes fixed, error messages working

--- models/Product.model.js
+++ models/Product.model.js
@@ -1,34 +1,38 @@
 const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
-const productSchema = new Schema({
-  title: {
-    type: String,
-  },
+const productSchema = new Schema(
+  {
+    title: {
+      type: String,
+    },
 
-  description: {
-    type: String,
-  },
+    description: {
+      type: String,
+    },
 
-  stock: {
-    type: Number,
-  },
+    stock: {
+      type: Number,
+    },
 
-  price: {
-    type: Number,
-  },
+    price: {
+      type: Number,
+    },
 
-  imageProduct:  {
-    type: String,
-  },
+    imageProduct: {
+      type: String,
+    },
 
-  
-  idOwner: {
-    type: Schema.Types.ObjectId,
-    reference: "User",
+    idOwner: {
+      type: Schema.Types.ObjectId,
+      reference: "User",
+    },
   },
-  
-});
+  {
+    // this second object adds extra properties: createdAt and updatedAt
+    timestamps: true,
+  }
+);
 
 const Product = model("Product", productSchema);
 
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -3,111 +3,96 @@ const Product = require("../models/Product.model");
 
 const router = require("express").Router();
 
-
-
 //POST
 
-
-router.post('/products',(req,res)=>{
-    const {title, description,stock, price ,imageProduct, userId} = req.body
-    if(!title || !description || !stock || !price){
-        res.json({message:"Please fill in all mandetory Fields"})
-        return
-    }
-    const idOwner=userId
-    Product.create({title, description,stock, price ,imageProduct, idOwner})
-    .then((newProduct)=>{
-        res.json({message:"Product Successfully Created"})
+router.post("/products", (req, res) => {
+  const { title, description, stock, price, imageProduct, userId } = req.body;
+  if (!title || !description || !stock || !price) {
+    res.status(400).json({ message: "Please fill in all mandatory Fields" });
+    return;
+  }
+  const idOwner = userId;
+  Product.create({ title, description, stock, price, imageProduct, idOwner })
+    .then((newProduct) => {
+      res.status(400).json({ message: "Product Successfully Created" });
     })
-    .catch((err)=>{
-        res.json(err)
-    })
-})
+    .catch((err) => {
+      console.error("Error creating product:", err);
+      res.status(500).json({ message: "Internal Server Error" });
+    });
+});
 
 //GET
-router.get('/products',isAuthenticated,(req,res)=>{
+router.get("/products", isAuthenticated, (req, res) => {
+  Product.find()
 
-    Product.find()
-    
-    .then((allProducts)=>{
-        res.json(allProducts)
-    
+    .then((allProducts) => {
+      res.json(allProducts);
     })
-    .catch((err)=>{
-        res.json(err)
-    })
-
-})
-
-
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 //find()
 //findByIdAndUpdate()
 //findByIdAndDelete()
 //
 
-
 //GET for 1 Product
 
-router.get('/products/:id',(req,res)=>{
-    const {id} = req.params
+router.get("/products/:id", (req, res) => {
+  const { id } = req.params;
 
-    Product.findById(id)
-   
-    .then((oneProduct)=>{
-        res.json(oneProduct)
+  Product.findById(id)
+
+    .then((oneProduct) => {
+      res.json(oneProduct);
     })
-    .catch((err)=>{
-        res.json(err)
-    })
-
-})
-
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 //PUT
 
-//Find a Product based on an id 
+//Find a Product based on an id
 //and then should change it based on what the user provides
 
-router.put('/products/:id',(req,res)=>{
-    const {id} = req.params
-    const {title, description,stock, price ,imageProduct} = req.body
-    
-    Product.findByIdAndUpdate(id,{title, description,stock, price ,imageProduct},{new:true})
-    .then((updatedProduct)=>{
-        res.json(updatedProduct)
+router.put("/products/:id", (req, res) => {
+  const { id } = req.params;
+  const { title, description, stock, price, imageProduct } = req.body;
+
+  Product.findByIdAndUpdate(
+    id,
+    { title, description, stock, price, imageProduct },
+    { new: true }
+  )
+    .then((updatedProduct) => {
+      res.json(updatedProduct);
     })
-    .catch(err=>{
-        res.json(err)
-    })
-
-})
-
-
-
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 //DELETE
 //create a route for deleting
 // once a request is sent the document with the id in the params should be deleted from our Database
 // send back as a response {message: "Product deleted"}
 
-router.delete('/products/:id',(req,res)=>{
-    
-    Product.findByIdAndDelete(req.params.id)
-    .then((deletedProduct)=>{
-        if(!deletedProduct){
-            res.json({message:"Product Not Found"})
-            return
-        }
-        res.json({message:"Product deleted"})
+router.delete("/products/:id", (req, res) => {
+  Product.findByIdAndDelete(req.params.id)
+    .then((deletedProduct) => {
+      if (!deletedProduct) {
+        res.json({ message: "Product Not Found" });
+        return;
+      }
+      res.json({ message: "Product deleted" });
     })
-    .catch((err)=>{
-        res.json(err)
-    })
-
-})
-
-
-
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 module.exports = router;
