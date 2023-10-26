199ec2205c87034ebee59abe8b0a3b253b6da73f
TiagoLobo-source <65892221+TiagoLobo-source@users.noreply.github.com>
2023-10-26 19:14:08 +0100

    still styling

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -2,6 +2,7 @@ const { isAuthenticated } = require("../middleware/jwt.middleware");
 const Product = require("../models/Product.model");
 
 const router = require("express").Router();
+const fileUploader = require("../config/cloudinary.config");
 
 //POST
 
@@ -29,6 +30,21 @@ router.post("/products", (req, res) => {
       console.error("Error creating product:", err);
       res.status(500).json({ message: "Internal Server Error" });
     });
+});
+
+// POST "/api/upload" => Route that receives the image, sends it to Cloudinary via the fileUploader and returns the image URL
+router.post("/upload", fileUploader.single("imageUrl"), (req, res, next) => {
+  // console.log("file is: ", req.file)
+ 
+  if (!req.file) {
+    next(new Error("No file uploaded!"));
+    return;
+  }
+  
+  // Get the URL of the uploaded file and send it as a response.
+  // 'fileUrl' can be any name, just make sure you remember to use the same when accessing it on the frontend
+  
+  res.json({ fileUrl: req.file.path });
 });
 
 //GET
